7a46363001d54559cce3a324cca15efd83a3c1b1
Justin Yoo <justin.yoo@aliencube.com>
2014-11-07 13:42:29 +1100

    Update Program to handle incoming messages

--- SourceCodes/02_Apps/SbeaSample.ConsoleApp/Program.cs
+++ SourceCodes/02_Apps/SbeaSample.ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
@@ -98,7 +99,7 @@ namespace SbeaSample.ConsoleApp
 
             try
             {
-                ProcessMessage();
+                ProcessRequests();
             }
             catch (Exception ex)
             {
@@ -112,86 +113,170 @@ namespace SbeaSample.ConsoleApp
                 File.AppendAllText(String.Format(_errorLogPath, DateTime.Today), sb.ToString());
             }
         }
-
-        private static void ProcessMessage()
+        private static void ProcessRequests()
         {
-            using (var conn = new SqlConnection(_csbSource.ToString()))
+            try
+            {
+                using (var conn = new SqlConnection(_csbSource.ToString()))
+                {
+                    conn.Open();
+
+                    bool messageReceived;
+                    do
+                    {
+                        messageReceived = false;
+                        using (var transaction = conn.BeginTransaction())
+                        using (var command = conn.CreateCommand())
+                        {
+                            command.Transaction = transaction;
+
+                            // Even if message_body is always XML, don't cast to XML inside the RECEIVE statement as this
+                            // may cause issues with activation. Convert/cast to XML after the RECEIVE is done.
+                            command.CommandText = String.Format("WAITFOR (RECEIVE TOP(1) conversation_handle, message_type_name, message_body FROM {0}.{1}), TIMEOUT {2}",
+                                                                BracketizeName(_messageQueueSchema),
+                                                                BracketizeName(_messageQueueName),
+                                                                _waitforTimeout);
+
+                            var reader = command.ExecuteReader();
+                            if (!reader.Read())
+                            {
+                                reader.Dispose();
+                                transaction.Commit();
+                                continue;
+                            }
+
+                            messageReceived = true;
+
+                            var conversationHandle = reader.GetGuid(0);
+                            var messageTypeName = reader.GetString(1);
+                            var messageBody = reader.GetSqlBinary(2);
+
+                            reader.Dispose();
+
+                            try
+                            {
+
+                                if (messageTypeName == _endDialogMessageType || messageTypeName == _errorMessageType)
+                                {
+                                    if (messageTypeName == _errorMessageType)
+                                    {
+                                        // Handle the error message
+                                    }
+                                }
+                                else
+                                {
+                                    using (var stream = new MemoryStream(messageBody.Value))
+                                    {
+                                        var message = XDocument.Load(stream);
+                                        var responsePayload = ProcessMessage(message);
+                                    }
+                                }
+                                EndConversation(conn, transaction, conversationHandle);
+                                transaction.Commit();
+                            }
+                            catch
+                            {
+                                EndConversation(conn, transaction, conversationHandle);
+                                transaction.Commit();
+                                throw;
+                            }
+                        }
+                    } while (messageReceived);
+                }
+            }
+            catch (Exception e)
+            {
+                var sb = new StringBuilder();
+                sb.AppendLine("======");
+                sb.AppendLine(e.Message);
+                sb.AppendLine();
+                sb.AppendLine(e.StackTrace);
+                sb.AppendLine("======");
+
+                File.AppendAllText(String.Format(_errorLogPath, DateTime.Today), sb.ToString());
+            }
+        }
+
+        private static void EndConversation(SqlConnection conn, SqlTransaction transaction, Guid conversationHandle)
+        {
+            using (var command = conn.CreateCommand())
+            {
+                command.Transaction = transaction;
+                command.CommandText = "END CONVERSATION @handle";
+                command.Parameters.Add(new SqlParameter("@handle", conversationHandle));
+                command.ExecuteNonQuery();
+            }
+        }
+        /// <summary>
+        /// Services request messages by doing all the necessary computation.
+        /// </summary>
+        private static string ProcessMessage(XDocument xml)
+        {
+            var changes = xml.Root;
+            var productId = Convert.ToInt32(changes.Element("ProductId").Value);
+            var trackingType = changes.Element("TrackingType").Value;
+            XElement inserted = null;
+            XElement deleted = null;
+            switch (trackingType)
+            {
+                case "INSERT":
+                    inserted = changes.Element("Inserted").Element("Row");
+                    break;
+                case "UPDATE":
+                    inserted = changes.Element("Inserted").Element("Row");
+                    deleted = changes.Element("Deleted").Element("Row");
+                    break;
+                case "DELETE":
+                    deleted = changes.Element("Deleted").Element("Row");
+                    break;
+                default:
+                    throw new InvalidOperationException("Invalid tracking type");
+            }
+
+            using (var conn = new SqlConnection(_csbTracking.ToString()))
             {
                 conn.Open();
 
-                bool messageReceived;
-                do
+                var fields = new List<string>() {"ProductId", "Name", "Description", "Price"};
+                foreach (var field in fields)
                 {
-                    messageReceived = false;
+                    object oldValue = null;
+                    object newValue = null;
+                    switch (trackingType)
+                    {
+                        case "INSERT":
+                            oldValue = Convert.DBNull;
+                            newValue = inserted.Element(field).Value;
+                            break;
+                        case "UPDATE":
+                            oldValue = deleted.Element(field).Value;
+                            newValue = inserted.Element(field).Value;
+                            break;
+                        case "DELETE":
+                            oldValue = deleted.Element(field).Value;
+                            newValue = Convert.DBNull;
+                            break;
+                        default:
+                            throw new InvalidOperationException("Invalid tracking type");
+                    }
 
                     using (var transaction = conn.BeginTransaction())
                     using (var command = conn.CreateCommand())
                     {
                         command.Transaction = transaction;
-                        command.CommandText =
-                            String.Format("WAITFOR (RECEIVE TOP(1) conversation_handle, message_type_name, message_body FROM {0}.{1}), TIMEOUT {2}",
-                                          BracketizeName(_messageQueueSchema), BracketizeName(_messageQueueName), _waitforTimeout);
-
-                        using (var reader = command.ExecuteReader())
-                        {
-                            if (reader.Read())
-                            {
-                                messageReceived = true;
-
-                                var conversationHandle = reader.GetGuid(0);
-                                var messageTypeName = reader.GetString(1);
-                                var messageBody = reader.GetSqlBinary(2);
-
-                                if (messageTypeName == _errorMessageType || messageTypeName == _endDialogMessageType)
-                                {
-                                    if (messageTypeName == _errorMessageType)
-                                    {
-                                        // Handle error messages.
-                                    }
-
-                                    using (var endConversationCommand = conn.CreateCommand())
-                                    {
-                                        endConversationCommand.Transaction = transaction;
-                                        endConversationCommand.CommandText = "END CONVERSATION @handle";
-                                        endConversationCommand.Parameters.Add(new SqlParameter("@handle", conversationHandle));
-                                        endConversationCommand.ExecuteNonQuery();
-                                    }
-                                }
-                                else
-                                {
-                                    try
-                                    {
-                                        using (var stream = new MemoryStream(messageBody.Value))
-                                        using (var sendCommand = conn.CreateCommand())
-                                        {
-                                            var xml = XDocument.Load(stream);
-                                            var result = SaveTrackingDetails(xml);
-
-                                            sendCommand.CommandText = "SEND ON CONVERSATION @handle MESSAGE TYPE [TrackingResponse] (@body)";
-                                            sendCommand.Parameters.Add(new SqlParameter("@handle", conversationHandle));
-                                            sendCommand.Parameters.Add(new SqlParameter("@body", result));
-                                            sendCommand.ExecuteNonQuery();
-                                        }
-                                    }
-                                    catch (Exception ex)
-                                    {
-                                        var sb = new StringBuilder();
-                                        sb.AppendLine("----------");
-                                        sb.AppendLine(ex.Message);
-                                        sb.AppendLine();
-                                        sb.AppendLine(ex.StackTrace);
-                                        sb.AppendLine("==========");
-
-                                        File.AppendAllText(String.Format(_errorLogPath, DateTime.Today), sb.ToString());
-                                        throw;
-                                    }
-                                }
-                            }
-                        }
+                        command.CommandText = "INSERT INTO [dbo].[TrackingLogs] ([Source], [Field], [TrackingType], [OldValue], [NewValue]) VALUES (@source, @field, @trackingType, @oldValue, @newValue)";
+                        command.Parameters.Add(new SqlParameter("@source", "[SourceDB].[dbo].[Products]"));
+                        command.Parameters.Add(new SqlParameter("@field", field));
+                        command.Parameters.Add(new SqlParameter("@trackingType", trackingType));
+                        command.Parameters.Add(new SqlParameter("@oldValue", oldValue));
+                        command.Parameters.Add(new SqlParameter("@newValue", newValue));
+                        command.ExecuteNonQuery();
                         transaction.Commit();
                     }
-                } while (messageReceived);
+                }
             }
+            // Send response back so that conversation can be closed
+            return "Audit Message processed!";
         }
 
         /// <summary>
@@ -200,12 +285,6 @@ namespace SbeaSample.ConsoleApp
         private static string BracketizeName(string sysname)
         {
             return String.Format("[{0}]", sysname.Replace("]", "]]"));
-        }
-
-        private static string SaveTrackingDetails(XDocument xml)
-        {
-
-            return "Saved";
         }
     }
 }
